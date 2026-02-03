613147f35408c3154bd3db9c4e7093530c9fd7e7
Saumay Gupta <148718955+Saumay-Gupta@users.noreply.github.com>
2026-02-03 17:28:23 +0530

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,38 @@
 import express from "express";
 import connectDB from "./db/connectDB.js";
-import urlRoute from "./routes/url.js";
 import cors from "cors"
+import dotenv from "dotenv"
+import { handleSignUP } from "./controllers/handleSignUP.js";
+import { handleLogin } from "./controllers/handleLogin.js";
+import { verifyUser } from "./middlewares/verifySession.js";
+import { handleOriginalUrl, handleRedirectUrl } from "./controllers/url.js";
+import { getAllUrls } from "./controllers/getAllUrls.js";
+import cookieParser from "cookie-parser";
+import handleDelete from "./controllers/handleDelete.js";
+
+dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,      
+  })
+);
 app.use(express.json());
 
 connectDB();
 
-app.use("/url", urlRoute);
+app.post('/signUP', handleSignUP);
+app.post('/login', handleLogin);
 
-app.listen(8001, ()=>{
+
+app.get('/url/all', verifyUser , getAllUrls);
+app.post('/url/shorten',verifyUser, handleOriginalUrl);
+app.get('/url/:shortID', handleRedirectUrl);
+app.delete('/url/:shortID', verifyUser, handleDelete);
+
+app.listen(5000, ()=>{
     console.log("Server Connected Succesfully..");
 })
 
